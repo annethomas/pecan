c6306e94e5c1f43ee35493e71002064b9cff35a7
annethomas <annethomas239@gmail.com>
2017-06-29 14:47:09 -0400

    Clean up hardcoded version

--- models/sipnet/R/write.configs.SIPNET.R
+++ models/sipnet/R/write.configs.SIPNET.R
@@ -329,48 +329,51 @@ write.config.SIPNET <- function(defaults, trait.values, settings, run.id, inputs
     IC.nc = nc_open(IC.path) #check for successful open?
     
     ## plantWoodInit gC/m2
-    plantWood = try(ncvar_get(IC.nc,"AbvGrndWood"),silent = TRUE)
+    plantWood = try(ncdf4::ncvar_get(IC.nc,"AbvGrndWood"),silent = TRUE)
     if (!is.na(plantWood) && is.numeric(plantWood)) {
-      param[which(param[, 1] == "plantWoodInit"), 2] <- sum(plantWood) * 1000 #MsTMIP AbvGrndWood kgC/m2
+      param[which(param[, 1] == "plantWoodInit"), 2] <- plantWood * 1000 #MsTMIP AbvGrndWood kgC/m2
     }
     else{
       #try back-calculate from LAI,sla, and total biomass? where is total biomass?
     }
     ## laiInit m2/m2
-    lai = try(ncvar_get(IC.nc,"LAI"),silent = TRUE)
-    if (!is.na(LAI) && is.numeric(LAI)) {
+    lai = try(ncdf4::ncvar_get(IC.nc,"LAI"),silent = TRUE)
+    if (!is.na(lai) && is.numeric(lai)) {
       param[which(param[, 1] == "laiInit"), 2] <- lai
     }
     ## litterInit gC/m2
-    litter = try(ncvar_get(IC.nc,"litter_carbon_content"),silent = TRUE)
+    litter = try(ncdf4::ncvar_get(IC.nc,"litter_carbon_content"),silent = TRUE)
     if (!is.na(litter) && is.numeric(litter)) {
       param[which(param[, 1] == "litterInit"), 2] <- litter * 1000 #CF litter_carbon_content kg/m2
     }
     ## soilInit gC/m2
-    soil = try(ncvar_get(IC.nc,"TotSoilCarb"),silent = TRUE)
+    soil = try(ncdf4::ncvar_get(IC.nc,"TotSoilCarb"),silent = TRUE)
     if (!is.na(soil) && is.numeric(soil)) {
       param[which(param[, 1] == "soilInit"), 2] <- sum(soil) * 1000 #MsTMIP TotSoilCarb kg C/m2
     }
     ## litterWFracInit fraction
-    litterWFrac = try(ncvar_get(IC.nc,"LitterMoistFrac"),silent = TRUE) #may or may not actually include in standard ic file
+    litterWFrac = try(ncdf4::ncvar_get(IC.nc,"LitterMoistFrac"),silent = TRUE) #may or may not actually include in standard ic file
     if (!is.na(litterWFrac) && is.numeric(litterWFrac)) {
       param[which(param[, 1] == "litterWFracInit"), 2] <- litterWFrac
     }
     ## soilWFracInit fraction
-    soilWFrac = try(ncvar_get(IC.nc,"SoilMoistFrac"),silent = TRUE)
+    soilWFrac = try(ncdf4::ncvar_get(IC.nc,"SoilMoistFrac"),silent = TRUE)
     if (!is.na(soilWFrac) && is.numeric(soilWFrac)) {
-      param[which(param[, 1] == "soilWFracInit"), 2] <- soilWFrac
+      param[which(param[, 1] == "soilWFracInit"), 2] <- sum(soilWFrac)
     }
     ## snowInit cm water equivalent
-    snow = try(ncvar_get(IC.nc,"SWE"),silent = TRUE)
+    snow = try(ncdf4::ncvar_get(IC.nc,"SWE"),silent = TRUE)
     if (!is.na(snow) && is.numeric(snow)) {
       param[which(param[, 1] == "snowInit"), 2] <- snow #MsTMIP SWE kg/m2, need to convert
     }
     ## microbeInit mgC/g soil
-    microbe = try(ncvar_get(IC.nc,"Microbial Biomass C"),silent = TRUE)
+    microbe = try(ncdf4::ncvar_get(IC.nc,"Microbial Biomass C"),silent = TRUE)
     if (!is.na(microbe) && is.numeric(microbe)) {
       param[which(param[, 1] == "microbeInit"), 2] <- microbe * .001 #BETY Microbial Biomass C mg C kg-1 soil
     }
+    
+    #close file
+    ncdf4::nc_close(IC.nc)
   }else{
     #some stuff about IC file that we can give in lieu of actual ICs
   }
--- modules/data.land/R/pool_ic_list2netcdf.R
+++ modules/data.land/R/pool_ic_list2netcdf.R
@@ -2,7 +2,7 @@
 ##' @title pool_ic_list2netcdf
 ##' @export
 ##'
-##' @param input list with two-elements: list of netcdf dimensions (dims; named values) and list of variables (vars; named values)
+##' @param input list with two elements: list of netcdf dimensions (dims, with named values) and list of variables (vals, with named values)
 ##' @param outdir directory to write netcdf file
 ##' @param siteid site id
 ##' @author Anne Thomas
@@ -22,27 +22,33 @@ pool_ic_list2netcdf <- function(input, outdir,siteid){
   dims2 <- list(lon,lat,nsoil)
   
   #hardcoded vars (assumes these are the desirable variables)
-  ic1 = ncvar_def(name = "AbvGrndWood", units = "kg C m-2", dim = dims1, -999)
-  ic2 = ncvar_def(name = "TotSoilCarb", units = "kg C m-2", dim = dims2, -999)
-  ic3 = ncvar_def(name = "litter_carbon_content", units = "kg C m-2", dim = dims1, -999) #made up
-  ic4 = ncvar_def(name = "LAI", units = "m2 m-2", dim = dims1, -999)
-  ic5 = ncvar_def(name = "SoilMoistFrac", units = "NA", dim = dims2, -999)
+  ic1 <- ncdf4::var_def(name = "AbvGrndWood", units = "kg C m-2", dim = dims1, -999)
+  ic2 <- ncdf4::var_def(name = "TotSoilCarb", units = "kg C m-2", dim = dims2, -999)
+  ic3 <- ncdf4::var_def(name = "litter_carbon_content", units = "kg C m-2", dim = dims1, -999) #made up
+  ic4 <- ncdf4::var_def(name = "LAI", units = "m2 m-2", dim = dims1, -999)
+  ic5 <- ncdf4::var_def(name = "SoilMoistFrac", units = "NA", dim = dims2, -999) #should this be dims2?
   #took out LitterMoistFrac (made up name, specific to Sipnet)
-  ic6 = ncvar_def(name = "CoarseWoodyDebris", units = "NA", dim = dims1, -999) #made up
-  ic7 = ncvar_def(name = "soilN", units = "mg kg-1 soil", dim = dims2, -999) #made up
-  ic8 = ncvar_def(name = "soilP", units = "mg kg-1 soil", dim = dims2, -999) #made up
-  ic9 = ncvar_def(name = "SWE", units = "kg m-2", dim = dims1, -999)
-  ic10 = ncvar_def(name = "Microbial Biomass C", units = "mg C kg-1 soil", dim = dims1, -999) #made up
+  ic6 <- ncdf4::var_def(name = "CoarseWoodyDebris", units = "NA", dim = dims1, -999) #made up
+  ic7 <- ncdf4::var_def(name = "soilN", units = "mg kg-1 soil", dim = dims2, -999) #made up
+  ic8 <- ncdf4::var_def(name = "soilP", units = "mg kg-1 soil", dim = dims2, -999) #made up
+  ic9 <- ncdf4::var_def(name = "SWE", units = "kg m-2", dim = dims1, -999)
+  ic10 <- ncdf4::var_def(name = "Microbial Biomass C", units = "mg C kg-1 soil", dim = dims1, -999) #made up
   
-  ncvars = list(ic1,ic2,ic3,ic4,ic5,ic6,ic7,ic8,ic9,ic10)
+  ncvars <- list(ic1,ic2,ic3,ic4,ic5,ic6,ic7,ic8,ic9,ic10)
   
   #possibility: dynamic vars (would assume R list has standard names; need a lookup table for dims and units)
-  
+  ncvars <- vector(mode = list, length = length(names(input$vals))) #more efficient than appending to list()
+  for(i in seq(input$vals)){
+    #look up names(input$vals)[[i]] in pecan standard output table, get units and dimensions
+    #var <- row from table
+    #dim <- list of dims based on table
+    #ncvars[[i]] <- ncdf4::var_def(name = var$name, units = var$units, dim = dim, -999)
+  }
   
   #create nc file
   str_ns <- paste0(siteid %/% 1e+09, "-", siteid %% 1e+09)
-  outfolder <- file.path(outdir, paste0("IC_site_", str_ns))
-  nc  <- ncdf4::nc_create(outfolder, ncvars)
+  outfile <- file.path(outdir, paste0("IC_site_", str_ns))
+  nc  <- ncdf4::nc_create(outfile, ncvars)
   
   #put variables in nc file
   for (i in seq(ncvars)) {
