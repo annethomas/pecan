be458df2d4cea994ad8ad62ef504acd3bbf178e6
annethomas <annethomas239@gmail.com>
2017-06-30 14:09:47 -0400

    clean up dynamic ncvar definition

--- modules/data.land/R/pool_ic_list2netcdf.R
+++ modules/data.land/R/pool_ic_list2netcdf.R
@@ -9,7 +9,7 @@
 
 pool_ic_list2netcdf <- function(input, outdir,siteid){
   ##define dimensions available for netcdf
-  #assuming dynamic dim names aren't necessary
+  #assuming additional dim names aren't necessary; could use process similar to ncvars if so
   lon <- ncdf4::ncdim_def("lon", "degrees_east", vals = input$dims$lon, 
                           longname = "station_longitude")
   lat <- ncdf4::ncdim_def("lat", "degrees_north", vals = input$dims$lat, 
@@ -17,40 +17,28 @@ pool_ic_list2netcdf <- function(input, outdir,siteid){
   nsoil <- ncdf4::ncdim_def(name = "nsoil", units = "cm", vals = input$dims$nsoil, 
                             longname = "depth to bottom of layer")
   
-  #sets of dims
-  dims1 <- list(lon, lat)
-  dims2 <- list(lon,lat,nsoil)
   dims <- list(lon = lon, lat = lat, nsoil = nsoil)
   
-  #hardcoded vars (assumes these are the desirable variables)
-  ic1 <- ncdf4::var_def(name = "AbvGrndWood", units = "kg C m-2", dim = dims1, -999)
-  ic2 <- ncdf4::var_def(name = "TotSoilCarb", units = "kg C m-2", dim = dims2, -999)
-  ic3 <- ncdf4::var_def(name = "litter_carbon_content", units = "kg C m-2", dim = dims1, -999) #made up
-  ic4 <- ncdf4::var_def(name = "LAI", units = "m2 m-2", dim = dims1, -999)
-  ic5 <- ncdf4::var_def(name = "SoilMoistFrac", units = "NA", dim = dims2, -999) #should this be dims2?
-  #took out LitterMoistFrac (made up name, specific to Sipnet)
-  ic6 <- ncdf4::var_def(name = "CoarseWoodyDebris", units = "NA", dim = dims1, -999) #made up
-  ic7 <- ncdf4::var_def(name = "soilN", units = "mg kg-1 soil", dim = dims2, -999) #made up
-  ic8 <- ncdf4::var_def(name = "soilP", units = "mg kg-1 soil", dim = dims2, -999) #made up
-  ic9 <- ncdf4::var_def(name = "SWE", units = "kg m-2", dim = dims1, -999)
-  ic10 <- ncdf4::var_def(name = "Microbial Biomass C", units = "mg C kg-1 soil", dim = dims1, -999) #made up
+  #lookup table
+  standard_vars <- data(standard_vars,package = "PEcAn.utils") #standard_vars replacing mstmip_vars; not yet merged
   
-  ncvars <- list(ic1,ic2,ic3,ic4,ic5,ic6,ic7,ic8,ic9,ic10)
-  
-  #possibility: dynamic vars (would assume R list has standard names; need a lookup table for dims and units)
-  ncvars <- vector(mode = list, length = length(names(input$vals))) #more efficient than appending to list()
-  standard_vars = read.csv("standard_vars.csv") #change
-  for(i in seq(input$vals)){
-    #look up variable in pecan standard output table, get units and dimensions
-    varname = names(input$vals)[[i]]
+  ###function to lapply to all variables in input list and return a list of ncvars
+  list_ncvars <- function(varname,standard_vars,dims){
+    print(varname)
     var <- standard_vars[which(standard_vars$Variable.Name == varname),]
-    #check var exists
-    
-    dimset = var[,c("dim1","dim2","dim3","dim4")]
+    #add: check var exists
+    dimset <- var[,c("dim1","dim2","dim3","dim4")]
     dim <- dims[which(names(dims) %in% dimset)] #subset list of all dims for this variable
-    #check that dim isn't 0
-    ncvars[[i]] <- ncdf4::var_def(name = var$Varaible.Name, units = var$Units, dim = dim, -999)
+    print(class(dim[[1]]))
+    #add: check that dim isn't 0
+    ncvar <- ncdf4::ncvar_def(name = varname, units = var$Units, dim = dim, -999) #also add longname?
+    if (var$Long.name != "na") {
+      ncvar$longname <- as.character(var$Long.name)
+    }
+    return(ncvar)
   }
+  
+  ncvars = lapply(names(IClist$vals),list_ncvars,standard_vars,alldims)
   
   #create nc file
   str_ns <- paste0(siteid %/% 1e+09, "-", siteid %% 1e+09)
@@ -59,14 +47,8 @@ pool_ic_list2netcdf <- function(input, outdir,siteid){
   
   #put variables in nc file
   for (i in seq(ncvars)) {
-    #print(i)
     varname <- ncvars[[i]]$name
-    if (varname %in% names(input$vals)){
-      ncdf4::ncvar_put(nc, ncvars[[i]], input$vals[[varname]])
-    }
-    else{
-      #will automatically add NA
-    }
+    ncdf4::ncvar_put(nc, ncvars[[i]], input$vals[[varname]])
   }
   
   #close file
